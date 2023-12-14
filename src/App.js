fac72b918b67c1f50bfb1ac498fd702290f7adb1
Sedjro2000 <manwenselove@gmail.com>
2023-12-14 21:43:53 +0100

    build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = process.env.REACT_APP_API_URL
+/*const API_URL = "https://www.omdbapi.com?apikey=b6003d8a"*/
+const API_URL = process.env.REACT_APP_API_URL;
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
